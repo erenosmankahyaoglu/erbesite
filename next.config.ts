1d55b2edd5dcfbcb04eb3df57f1dda8f3b532bb4
erenosmankahyaoglu <erenosmankahyaoglu@gmail.com>
2026-03-13 18:02:52 +0300

    update hersey

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,45 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/Urunlerimiz-kesiciler.html",
+        destination: "/hizmetlerimiz",
+        permanent: true,
+      },
+      {
+        source: "/Urunlerimiz-bantlar.html",
+        destination: "/hizmetlerimiz",
+        permanent: true,
+      },
+      {
+        source: "/Urunlerimiz-is-guvenligi.html",
+        destination: "/hizmetlerimiz",
+        permanent: true,
+      },
+      {
+        source: "/Urunlerimiz-slikon-tabancalari.html",
+        destination: "/hizmetlerimiz",
+        permanent: true,
+      },
+      {
+        source: "/Urunlerimiz-kopukler-ve-slikonlar.html",
+        destination: "/hizmetlerimiz",
+        permanent: true,
+      },
+      {
+        source: "/Urunlerimiz-cizmeler-ve-botlar.html",
+        destination: "/hizmetlerimiz",
+        permanent: true,
+      },
+      {
+        source: "/Urunlerimiz.html",
+        destination: "/hizmetlerimiz",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
